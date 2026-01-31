f8f6999eb6fbc127389bb2348f0e3c1139978428
doooniel-3068 <dzhu686@gmail.com>
2026-01-31 20:23:18 +1000

    implemented insertion and deletion on gap buffer

--- src/buffer.rs
+++ src/buffer.rs
@@ -16,7 +16,7 @@ impl GapBuffer {
 
     pub fn from_str(text: &str) -> Self {
         let mut vec = Vec::from(text.as_bytes());
-        vec.resize(vec.len() + 32, 0);
+        vec.resize(vec.len() + 64, 0);
         let len = text.len();
 
         GapBuffer {
@@ -35,17 +35,82 @@ impl GapBuffer {
     }
 
     pub fn to_string(&self) -> String {
-        
-        // before goes from o = gap_start -1
-        //
-        // after goes from gap_end to buffer len
-        
+
         let mut result = Vec::new();
         result.extend(&self.buffer[0..self.gap_start]);
+
         result.extend(&self.buffer[self.gap_end..]);
 
         String::from_utf8_lossy(&result).to_string()
 
+    }
+
+    pub fn grow_gap(&mut self){
+        let current_gap = self.gap_end - self.gap_start;
+        let new_gap = current_gap * 2;
+        let bytes_to_add = new_gap - current_gap;
+
+        // insert empty space into gap to make room
+        self.buffer.splice(self.gap_end..self.gap_end,vec![0; bytes_to_add]);
+        self.gap_end += bytes_to_add;
+    }
+
+    pub fn move_gap(&mut self, pos: usize){
+
+        if pos < self.gap_start {
+            let amount = self.gap_start - pos;
+            let destination = self.gap_end - amount;
+
+            self.buffer.copy_within(pos..self.gap_start, destination);
+
+            self.gap_start = pos;
+            self.gap_end = destination;
+             
+        }
+
+        else if pos>self.gap_start {
+            let amount = pos - self.gap_start;
+            let src_start = self.gap_end;
+            let src_end = self.gap_end + amount;
+
+            self.buffer.copy_within(src_start..src_end, self.gap_start);
+
+            self.gap_start = pos;
+            self.gap_end = src_end;
+        }
+
+    }
+
+
+
+    pub fn insert(&mut self, pos:usize, ch:char){
+        self.move_gap(pos);
+        if self.gap_start == self.gap_end {
+            self.grow_gap();
+        }
+        let mut b = [0; 4];
+        let ch_bytes = ch.encode_utf8(&mut b).as_bytes();
+        self.buffer[self.gap_start..self.gap_start + ch_bytes.len()].copy_from_slice(ch_bytes);
+        self.gap_start += ch_bytes.len();
+    }
+
+    pub fn delete_range(&mut self, start:usize, end:usize) -> String {
+        self.move_gap(start);
+        let actual_end = self.gap_end - self.gap_start + end;
+        let deleted_text = String::from_utf8_lossy(&self.buffer[self.gap_end..actual_end]).to_string();
+        self.gap_end = actual_end;
+        deleted_text
+    }
+
+    pub fn insert_str(&mut self, pos: usize, text: &str){
+        let textlen = text.len();
+        let text_bytes = text.as_bytes();
+        self.move_gap(pos);
+        while (self.gap_end-self.gap_start)< textlen {
+            self.grow_gap();
+        }
+        self.buffer[self.gap_start..self.gap_start+textlen].copy_from_slice(text_bytes);
+        self.gap_start += textlen;
     }
 
     // getters
@@ -60,6 +125,10 @@ impl GapBuffer {
 
     pub fn get_gap_end(&self) -> usize {
         self.gap_end
+    }
+
+    pub fn print_internal_state(&self){
+        println!("{},{},{}", self.get_buffer_len(), self.get_gap_start(), self.get_gap_end());
     }
 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
 mod buffer;
 use buffer::GapBuffer;
-fn main() {       
-    // create empty buffer and verify length is 0 
-    let buf1 = GapBuffer::new();
-    println!("{}", buf1.len());
-
-    // create a buffer from hello, verify length is 5
-    let buf2 = GapBuffer::from_str("hello");
-    println!("{}", buf2.len());
-
-    println!("{}", buf2.to_string());
-
-    println!("{},{},{}", buf2.get_buffer_len(), buf2.get_gap_start(), buf2.get_gap_end());
+fn main() {
+    println!("=== Checkpoint 4.2 Tests ===\n");
+    
+    // Test 1: Create buffer with "Hello"
+    let mut buffer = GapBuffer::from_str("Hello");
+    println!("1. Initial buffer: '{}'", buffer.to_string());
+    
+    buffer.insert(0, 'X');
+    println!("2. After insert 'X' at 0: '{}'", buffer.to_string());
+    
+    buffer.insert(3, 'Y');
+    println!("3. After insert 'Y' at 3: '{}'", buffer.to_string());
+    
+    let deleted = buffer.delete_range(1, 3);
+    println!("4. After delete range (1,3): '{}'", buffer.to_string());
+    println!("   Deleted text: '{}'", deleted);
+    
+    buffer.insert_str(5, "WORLD");
+    println!("5. After insert 'WORLD' at 5: '{}'", buffer.to_string());
+    
 }
 
